769db22fdbafddbf64230bb6c3cccf618ec39433
wangtao <1182792703@qq.com>
2021-06-15 23:42:59 +0800

    修改为vue

--- src/app1.js
+++ src/app1.js
@@ -2,65 +2,47 @@ import $ from "jquery";
 import "./app1.css";
 import Model from "./base/Model";
 import View from "./base/View";
-
-
-
-// 数据放置到m
-const m = new Model({data:  {
-    n: parseInt( localStorage.getItem("n") ),
-},
-update(data){
-    Object.assign(m.data, data);
-    this.trigger('m:updated');
-    localStorage.setItem('n', data.n)
-}
-});
+import Vue from 'vue';
 
 // 其他为c
 
 const init = (el) => {
-    new View( {
+    new Vue( {
         el: el,
-        data: m.data,
-        html: `
-        <div>
+        data: {n: parseInt( localStorage.getItem("n") )},
+        methods: {
+            add() {
+                this.n++;
+            },
+            minus() {
+                this.n--;
+            },
+            mul(){
+                this.n *= 2;
+            },
+            div() {
+                this.n /= 2;
+            },
+        },
+        watch:{
+            n(){
+                localStorage.setItem('n', this.n)
+            }
+        },
+        template: `
+        <section>
             <div class="output">
                 <span id="number">{{n}}</span>
             </div>
             <div>
-                <button id="add1">+1</button>
-                <button id="minus1">-1</button>
-                <button id="mul2">*2</button>
-                <button id="div">÷2</button>
+                <button @click="add">+1</button>
+                <button @click="minus">-1</button>
+                <button @click="mul">*2</button>
+                <button @click="div">÷2</button>
             </div>
-        </div>
+        </section>
         `,
-        render (n) {
-            n = n.n;
-            if(this.el.children.length !== 0){
-                this.el.empty()
-            }
-            $(this.html.replace('{{n}}', n)).prependTo($(this.el))
-            
-        },
-        events: {
-            'click #add1': 'add',
-            'click #minus1': 'minus',
-            'click #mul2': 'mul',
-            'click #div': 'div',
-        },
-        add() {
-            m.update({n: m.data.n + 1})
-        },
-        minus() {
-            m.update({n: m.data.n - 1})
-        },
-        mul(){
-            m.update({n: m.data.n * 2})
-        },
-        div() {
-            m.update({n: m.data.n / 2})
-        },
+
     })
 }
 export default init;
--- src/app2.js
+++ src/app2.js
@@ -1,70 +1,29 @@
-import $ from "jquery";
-import "./app2.css";
-import EventBus from "./base/EventBus";
-import Model from "./base/Model";
-import View from "./base/View";
-const eventBus = new EventBus();
 
-const localKey = 'app2.index';
-// 数据放置到m
-const m = new Model(
-    {
-    data: {
-        index: parseInt( localStorage.getItem(localKey) || 0),
-    },
-    create() {},
-    delete() {},
-    update(data) {
-        Object.assign(m.data, data);
-        eventBus.trigger('m:updated');
-        localStorage.setItem(localKey, data.index)
-    }
-})
+import "./app2.css";
+import Vue from "vue"
 
 // 其他为c
 const init = (el) => {
-    new View({
+    new Vue({
         el: el,
-        data: m.data,
-        eventBus: eventBus,
-        html: (index) => { return `
-        <div>
+        data: {
+            index: 0
+        },
+        methods: {
+
+        },
+        template: `
+        <section id="app2">
             <ol class="tab-bar">
-                <li class="${index === 0 ? 'selected': ''}" data-index=0>1</li>
-                <li class="${index === 1 ? 'selected': ''}" data-index=1>2</li>
+                <li @click="index=0"  :class="index === 0 ? 'selected' : ''">1</li>
+                <li  @click="index=1" :class="index === 1 ? 'selected' : ''">2</li>
             </ol>
             <ul class="tab-content">
-                <li class=${index === 0 ? 'active': ''}>内容一</li>
-                <li class=${index === 1 ? 'active': ''}>内容二</li>
+                <li :class="index === 0 ? 'active' : ''">内容一</li>
+                <li :class="index === 1 ? 'active' : ''">内容二</li>
             </ul>
-        </div>
-        `
-        },
-        render ({index}) {
-            if(this.el.children.length !== 0){
-                this.el.empty()
-            }
-            $(this.html(parseInt(index))).prependTo($(this.el))
-            
-        },
-        // init(el) {
-        //     this.el = $(el);
-        //     this.render(m.data.index);
-        //     this.autoBindEvents();
-        //     eventBus.on(
-        //         'm:updated',
-        //         () => {
-        //             this.render(m.data.index);
-        //         }
-        //     )
-        // },
-        events: {
-            'click .tab-bar li': 'changeStatus',
-        },
-        changeStatus(e) {
-            const index = e.target.dataset.index
-            m.update({index: index})
-        },
+        </section>
+        `,
     })
 }
 export default init;
